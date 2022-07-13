3dcc03d313fe91ae1d077114509036f7e98104f3
BILLI ALDIAS <108719608+Playshopbot@users.noreply.github.com>
2022-07-13 13:29:31 +0700

    Update general-menu2.js

--- plugins/general-menu2.js
+++ plugins/general-menu2.js
@@ -2,7 +2,7 @@ import moment from 'moment-timezone'
 let handler = async (m, { conn, usedPrefix, __dirname, text, command }) => {
 let date = moment.tz('Asia/Jakarta').format("dddd, Do MMMM, YYYY")
 let time = moment.tz('Asia/Jakarta').format('HH:mm:ss')
-conn.sendHydrated(m.chat, '*『 INTRODUCTION 』*', `Ingin sewabot murah 3.000 permanen silakan hubungi owner kami untuk sewa bot\nSilakan klik tombol menu untuk memulainya\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://bit.ly/playshop1', 'INSTAGRAM', null, null, [
+conn.sendHydrated(m.chat, '*『 INTRODUCTION 』*', `Hai kak, sewabot murah permanen 3.000 silakan hubungi kami https://wa.me/message/AN7LRLW5PDJTP1 \nSilakan klik tombol menu untuk memulainya\n\nᴅᴀᴛᴇ: ${date}\nᴛɪᴍᴇ: ${time} ﹙ɢᴍᴛ +7:00)`.trim(), './media/broadcast.jpg', 'https://bit.ly/playshop1', 'INSTAGRAM', null, null, [
 [`Menu`, '.craft'],
 [`\nAku gila dan aku bangga:v`, 'Bilek']
 ], m, {asLocation: true})
